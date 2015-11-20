8ca9a88a8ae68dbc9c3c9692e920b50ab909e3fb
root <root@srv-dev.intra.misiv.fr>
2015-11-20 17:28:05 +0100

    add param helpzone to helpzonebeforeshow event

--- jquery.helpzone.js
+++ jquery.helpzone.js
@@ -98,9 +98,9 @@
             $.extend(inst.options, options); // update with new options 
             input.on(inst.options.event + '.' + this.propertyName, function () {
                 if ($.isFunction(inst.options.beforeUpdate)) { // call custom event handler before update
-                    inst.options.beforeUpdate.call(input, plugin._beforeUpdateEvent);
+                    inst.options.beforeUpdate.call(input, plugin._beforeUpdateEvent, inst.options.zone);
                 }
-                input.trigger(plugin._beforeUpdateEvent); // trigger ou custom event before update
+                input.trigger(plugin._beforeUpdateEvent, inst.options.zone); // trigger ou custom event before update
                 if (!plugin._beforeUpdateEvent.isDefaultPrevented()) { // if not prevented
                     plugin._updateHelpZoneContent(inst.options.zone, inst.options.content(input));
                 }
